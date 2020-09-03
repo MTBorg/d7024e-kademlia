81b6e253ee8845355363890a1ce70d9dc16f102e
Martin Terneborg <martinterneborg@protonmail.com>
2020-09-03 14:58:28 +0200

    Implement hot reloading
    This commit restructures the docker files, there is now a separate dev
    and production dockerfile/compose-file. Hotreloading is done with
    codedaemon which has been added as a dependency.
    The project folder has been added as a volume in order to detect file changes.
    This means that the compiled binaries will show up on the host machine,
    but as these should not be committed, they have been moved to a
    gitignored directory bin.

--- internal/command/listener/listener.go
+++ internal/command/listener/listener.go
@@ -4,42 +4,58 @@ import (
 	"github.com/rs/zerolog/log"
 	"kademlia/internal/command/parser"
 	"net"
+	"os"
 )
 
+// Clears the socket at socketAddress
+//
+// Useful for making sure a socket does not try to connect to an already
+// bound address.
+// Works by removing the file specified by the specified socket address.
+func ClearSocket(socketAddress string) {
+	log.Debug().Str("SocketAddress", socketAddress).Msg("Clearing socket")
+	if err := os.RemoveAll(socketAddress); err != nil {
+		log.Error().Str("SocketAddress", socketAddress).Msg("Failed to clear socket")
+	} else {
+		log.Debug().Str("SocketAddress", socketAddress).Msg("Socket cleared")
+	}
+}
+
 func respond(c net.Conn) {
-	for {
-		buf := make([]byte, 512)
-		nr, err := c.Read(buf)
-		if err != nil {
-			return
-		}
+	buf := make([]byte, 512)
+	nr, err := c.Read(buf)
+	if err != nil {
+		return
+	}
 
-		data := buf[0:nr]
-		log.Info().Str("Command", string(data)).Msg("Received command")
+	data := buf[0:nr]
+	log.Info().Str("Command", string(data)).Msg("Received command")
 
-		command := cmdparser.ParseCmd(string(data))
+	command := cmdparser.ParseCmd(string(data))
 
-		// Execute command
-		var executionResult string
-		if command != nil {
-			executionResult, err = command.Execute()
+	// Execute command
+	var executionResult string
+	if command != nil {
+		executionResult, err = command.Execute()
 
-			// Write response
-			if err == nil {
-				log.Debug().Str("Message", executionResult).Msg("Sending response")
-				_, err = c.Write([]byte(executionResult))
-				if err != nil {
-					log.Error().Msgf("Failed to write response: %s", err)
-				}
+		// Write response
+		if err == nil {
+			log.Debug().Str("Message", executionResult).Msg("Sending response")
+			_, err = c.Write([]byte(executionResult))
+			if err != nil {
+				log.Error().Msgf("Failed to write response: %s", err)
 			}
 		}
-
-		c.Close()
 	}
+
+	c.Close()
 }
 
 func Listen() {
 	const socketAddress = "/tmp/echo.sock"
+
+	ClearSocket(socketAddress)
+
 	l, err := net.Listen("unix", socketAddress)
 	if err != nil {
 		log.Error().Msgf("Failed to listen: %s", err)
@@ -49,11 +65,11 @@ func Listen() {
 
 	for {
 		c, err := l.Accept()
-		log.Info().Str("Address", c.LocalAddr().String()).Msg("Received message from socket")
-		if err != nil {
+		if err == nil {
+			log.Info().Str("Address", socketAddress).Msg("Received message from socket")
+			go respond(c)
+		} else {
 			log.Error().Msgf("Listener failed to accept: %s", err)
 		}
-
-		go respond(c)
 	}
 }
