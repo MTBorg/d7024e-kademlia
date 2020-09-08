689588c04488770e2e770b4fe92e0a730382c387
Aron Strandberg <arostr-5@student.ltu.se>
2020-09-08 15:52:42 +0200

    Add RPC PING
    IPC parses ping and sends a PING message: "PING" with <random kademlia id>, receving node responds with a PONG message: "PING" and <the random kademlia id>, both nodes updates their bucket
    corresponding to the contacting node with node id and address

--- internal/commands/ping/ping.go
+++ internal/commands/ping/ping.go
@@ -2,6 +2,7 @@ package ping
 
 import (
 	"errors"
+	"kademlia/internal/network"
 
 	"github.com/rs/zerolog/log"
 )
@@ -12,16 +13,15 @@ type Ping struct {
 
 func (p Ping) Execute() (string, error) {
 	log.Debug().Str("Target", p.Target).Msg("Executing ping command")
+	network.Net.SendPingMessage(p.Target)
 
-	// TODO: Implement ping
-	result := "Not yet implemented"
+	return "Ping sent!", nil
 
-	return result, nil
 }
 
 func (p *Ping) ParseOptions(options []string) error {
 	if len(options) < 1 {
-		return errors.New("Missing target address")
+		return errors.New("Missing nodeID and target address")
 	}
 	p.Target = options[0]
 	return nil
--- internal/rpc/rpc_test.go
+++ internal/rpc/rpc_test.go
@@ -1,10 +1,10 @@
-package message_test
+package rpc_test
 
 import (
 	"errors"
 	"fmt"
 	"kademlia/internal/kademliaid"
-	"kademlia/internal/message"
+	"kademlia/internal/rpc"
 	"strings"
 	"testing"
 
@@ -23,50 +23,50 @@ func (m *SenderMock) Send(data string) error {
 
 func TestNew(t *testing.T) {
 	var content, target = "some message", "127.0.0.1:1337"
-	msg := message.New(content, target)
+	rpc := rpc.New(content, target)
 
-	assert.Equal(t, msg.Target, target)
-	assert.Equal(t, msg.Content, content)
+	assert.Equal(t, rpc.Target, target)
+	assert.Equal(t, rpc.Content, content)
 }
 
 func TestDeserialize(t *testing.T) {
-	var msg message.Message
+	var r rpc.RPC
 	var err error
 	// Should return an empty message and error if the string is empty
-	msg, err = message.Deserialize("")
-	assert.Empty(t, msg)
+	r, err = rpc.Deserialize("")
+	assert.Empty(t, r)
 	assert.Error(t, err)
 
 	// Should return an empty message and error if the string only contains a
 	// sender id and no separator
-	msg, err = message.Deserialize("senderid")
-	assert.Empty(t, msg)
+	r, err = rpc.Deserialize("senderid")
+	assert.Empty(t, r)
 	assert.Error(t, err)
 
 	// Should be able to pass empty content
-	msg, err = message.Deserialize("senderid;")
+	r, err = rpc.Deserialize("senderid;rpcid;")
 	assert.NoError(t, err)
-	assert.Equal(t, msg.Content, "")
+	assert.Equal(t, r.Content, "")
 }
 
 func TestSend(t *testing.T) {
 	testId := strings.Repeat("1", 40) //IDs are 160-bit (= 40 hex characters)
 	var senderMock *SenderMock
-	msg := message.Message{SenderId: kademliaid.FromString(testId), Content: "content", Target: "target"}
-	msgSerialized := fmt.Sprintf("%s;content", testId)
+	rpc := rpc.RPC{SenderId: kademliaid.FromString(testId), RPCId: kademliaid.FromString(testId), Content: "content", Target: "target"}
+	rpcSerialized := fmt.Sprintf("%s;%s;content", testId, testId)
 	var err error
 
 	// Should return the error from send if there was an error
 	senderMock = new(SenderMock)
-	senderMock.On("Send", msgSerialized).Return(errors.New("this is an error"))
-	err = msg.Send(senderMock)
+	senderMock.On("Send", rpcSerialized).Return(errors.New("this is an error"))
+	err = rpc.Send(senderMock)
 	assert.Equal(t, err, errors.New("this is an error"))
 	senderMock.AssertExpectations(t)
 
 	// Should return nil if send does not return an error
 	senderMock = new(SenderMock)
-	senderMock.On("Send", msgSerialized).Return(nil)
-	err = msg.Send(senderMock)
+	senderMock.On("Send", rpcSerialized).Return(nil)
+	err = rpc.Send(senderMock)
 	assert.NoError(t, err)
 	senderMock.AssertExpectations(t)
 }
