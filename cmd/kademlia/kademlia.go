f05ff3b31e2a02544058b5304b8ffb1ebab8f17f
banunkers <hugwan-6@student.ltu.se>
2020-09-09 14:39:25 +0200

    Seperate Kademlia node from main
    Moved the kademlia node type and methods from the main file and created
    a global variable containing the node. This Node variable is initiliazed
    when the node is srated in the main function (currently only holds the
    node id).

--- cmd/kademlia/kademlia.go
+++ cmd/kademlia/kademlia.go
@@ -1,33 +1,22 @@
 package main
 
 import (
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"kademlia/internal/command/listener"
-	. "kademlia/internal/contact"
 	"kademlia/internal/message"
+	. "kademlia/internal/node"
 	"os"
 	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
-
-type Kademlia struct {
-}
-
-func (kademlia *Kademlia) LookupContact(target *Contact) {
-	// TODO
-}
-
-func (kademlia *Kademlia) LookupData(hash string) {
-	// TODO
-}
-
-func (kademlia *Kademlia) Store(data []byte) {
-	// TODO
-}
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 	log.Info().Msg("Starting node...")
+
+	KadNode.Init()
+	log.Info().Str("NodeID", KadNode.Id.String()).Msg("ID assigned")
 
 	go cmdlistener.Listen()
 	msglistener.Listen()
