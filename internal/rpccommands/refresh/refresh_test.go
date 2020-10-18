b62935a13ca662658a06584bb06cc366d6fae2b5
banunkers <hugwan-6@student.ltu.se>
2020-10-18 11:44:20 +0200

    Add test for REFRESH RCP methods

--- internal/rpccommands/refresh/refresh_test.go
+++ internal/rpccommands/refresh/refresh_test.go
@@ -1,20 +1,38 @@
 package refresh_test
 
 import (
+	"kademlia/internal/address"
+	"kademlia/internal/kademliaid"
+	"kademlia/internal/node"
+	"kademlia/internal/rpccommands/refresh"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestExecute(t *testing.T) {
-	// TODO: Hugo knows the way
+	r := refresh.Refresh{}
+	n := node.Node{}
+	addr := address.New("127.0.0.1:1234")
+	n.Init(addr)
+	val := "hello world"
+	hash := kademliaid.NewKademliaID(&val)
 
+	// should not fail if the value is not stored in the nodes datastore
+	r.ParseOptions(&[]string{hash.String()})
+	r.Execute(&n)
 }
 
 func TestParseOption(t *testing.T) {
-	// TODO: Hugo knows the way
+	r := refresh.Refresh{}
+	val := "hello world"
+	hash := kademliaid.NewKademliaID(&val)
 
-}
+	// should parse the hash
+	err := r.ParseOptions(&[]string{hash.String()})
+	assert.Nil(t, err)
 
-func TestPrintUsage(t *testing.T) {
-	// should be equal
-
+	// should return an error when the hash is missing
+	err = r.ParseOptions(&[]string{})
+	assert.NotNil(t, err)
 }
