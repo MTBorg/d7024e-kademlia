65649befb59f6259291d01beb03e1b8058feffaf
Martin Terneborg <martinterneborg@protonmail.com>
2020-10-19 16:46:08 +0200

    Add test for findnoderesp

--- internal/rpccommands/findnoderesp/findnoderesp_test.go
+++ internal/rpccommands/findnoderesp/findnoderesp_test.go
@@ -1,8 +1,12 @@
 package findenoderesp_test
 
 import (
+	"kademlia/internal/datastore"
 	"kademlia/internal/kademliaid"
+	"kademlia/internal/node"
+	"kademlia/internal/nodedata"
 	"kademlia/internal/rpccommands/findnoderesp"
+	"kademlia/internal/rpcpool"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,6 +19,25 @@ func TestNew(t *testing.T) {
 	// should return a FindNode object
 	fnResp = findenoderesp.New(rpcId)
 	assert.IsType(t, &findenoderesp.FindNodeResp{}, fnResp)
+}
+
+func TestExecute(t *testing.T) {
+	n := node.Node{NodeData: nodedata.NodeData{DataStore: datastore.New(), RPCPool: rpcpool.New()}}
+	rpcID := kademliaid.NewRandomKademliaID()
+	n.RPCPool.Add(rpcID)
+	findRespCmd := findenoderesp.New(rpcID)
+	var content, data string
+	var channel chan string
+
+	// Should return just the content of the rpc if the value was not found
+	content = "some content"
+	findRespCmd.ParseOptions(&[]string{content})
+	go func() {
+		findRespCmd.Execute(&n)
+	}()
+	channel = n.RPCPool.GetEntry(rpcID).Channel
+	data = <-channel
+	assert.Equal(t, content, data)
 }
 
 func TestParseOptions(t *testing.T) {
